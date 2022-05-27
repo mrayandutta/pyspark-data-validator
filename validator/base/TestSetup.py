015179c78af0aea0d7d2ea0680c4374288ee8262
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-05-27 21:50:54 +0530

    Added config file

--- validator/base/TestSetup.py
+++ validator/base/TestSetup.py
@@ -1,4 +1,18 @@
-from pyspark.sql import SparkSession
+import findspark
 
-spark_session = SparkSession.builder.appName("test").getOrCreate()
-spark_session.createDataFrame([(1, "value1"), (2, "value2")], ["id", "value"]).show()
+findspark.init()
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
+from pyspark.sql.functions import avg
+
+spark = SparkSession.builder.appName("test").getOrCreate()
+
+columns = ["id","salary","department"]
+data = [(1,1000,"IT"), (2,1500,"HR"), (3,500,"Marketing")]
+rdd = spark.sparkContext.parallelize(data)
+df = rdd.toDF(columns)
+df.show()
+win_spec_by_department = Window.partitionBy('department')
+avg_salary_df = df.withColumn("average salary",avg("salary").over(win_spec_by_department))
+avg_salary_df.show()
